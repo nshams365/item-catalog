7811cbef335a07791807ea081168dcdc043726da
nshams365@gmail.com <TSA@home-mbp.home>
2019-07-01 23:05:49 +0100

    templates files are modified and so app.py file

--- app.py
+++ app.py
@@ -1,28 +1,58 @@
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Module Imports
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Category, Title
+from setup_app_db import *
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Application instance
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///catalog.db')
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Flask instance
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# DB connection
+engine = create_engine('sqlite:///book_catalog.db')
 Base.metadata.bind = engine
 
+# Session instance 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-@app.route('/')
-@app.route('/index')
-def index():
-	categories = session.query(Category).all()
-	#titles = session.query(Title.filter_by(title_id=title.id))
-	return render_template('index.html', categories=categories)
-	#output = ''
-	# for i in categories:
-	# 	output += i.name
-	# 	output += '</br>'
-	# return output
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Application routing
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+# Application Home
+@app.route('/')
+@app.route('/library/')
+def showLibrary():
+	categories = session.query(Category).order_by(asc(Category.cat_name))
+	titles = session.query(Title).order_by(desc(Title.t_id)).limit(5)
+	return render_template('library.html', categories=categories, titles=titles)
+
+# Books in a Category
+
+@app.route('/library/<path:category_name>')
+@app.route('/library/<path:category_name>/books/')
+def showCategoryBooks(category_name):
+	category = session.query(Category).filter_by(cat_name=category_name).one()
+	titles = session.query(Title).filter_by(category=category).all()
+	return render_template('books.html', category=category.cat_name, titles=titles)
+
+
+
+# Book details
+
+
+
+	
 
 # @app.route('/categories/<int:category_id>/new/', methods=['GET','POST'])
 # def newEntry(category_id):
